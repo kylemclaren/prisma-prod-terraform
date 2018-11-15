ec034c55e46bf71720655695ab37cb2f517fd441
kylemclaren <kyle.mclaren1@gmail.com>
2018-11-15 16:33:14 +0200

    optional persistent state backend

---
+++ aws/backend.tf
@@ -0,0 +1,10 @@
+# Uncomment if you wish to use a backend to store tfstate (useful for distributed teams or integrating with CI pipelines)
+
+# terraform {
+#   backend "s3" {
+#     bucket = "terra-state-bucket"
+#     key    = "tfstate"
+#     region = "us-west-2"
+#   }
+# }
+
